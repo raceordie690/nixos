dd2036378a62f490601cde0c8297f6860d9cf317
Robert Purdom <robert.purdom@intelafy.com>
2025-10-02 17:35:36 -0500

    incorporating nixbeast

--- modules/roles/desktop-gnome.nix
+++ modules/roles/desktop-gnome.nix
@@ -1,9 +1,9 @@
 { config, pkgs, ... }:
 {
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+#  services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
+#  services.displayManager.gdm.enable = true;
+#  services.desktopManager.gnome.enable = true;
 }
